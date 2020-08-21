e6b8553db6ab2f5f5e9e6c4313c73f388ed02ff1
Harshal <49345924+Harshal-Vaidya@users.noreply.github.com>
2020-08-22 00:24:26 +0530

    Version 2 made parameters configurable

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
+import pymysql
+# ------------------------------------------------this has been added after making parameters configurable
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
+
 app = Flask(__name__)
+if (local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+# ----------------------------------
+
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
 
 @app.route("/home")
 def main():
-    return render_template('index.html')
+    return render_template('index.html',params=params)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name=name, phone_num=phone, message=message, date=datetime.now(), email=email)
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html',params=params)
+
 
 @app.route("/post")
 def post():
-    return render_template('post.html')
+    return render_template('post.html',params=params)
 
 
 app.run(debug=True)
